fc1c52b7627598f224c3921aa4e3e9bbd971f3bc
shencheng <shencheng_1014@aliyun.com>
2024-02-27 14:21:08 +0800

    再次尝试2

--- privateCrypt.py
+++ privateCrypt.py
@@ -43,6 +43,7 @@ def decrypt_aes_ecb(ciphertext, key):
     print('----------------')
     print(ciphertext, ' + ', cipher.decrypt(ciphertext), ' + ', AES.block_size)
     print('----------------')
+    plain_str = None
     try:
         plain_str = unpad(cipher.decrypt(ciphertext), AES.block_size)
     except Exception as e:
